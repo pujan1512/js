3794978b25aa890498140b21c119e5834b32df0e
Pujan Patel <pujan.patel15122006@gmail.com>
2024-11-10 08:14:31 +0000

    container

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "Pujan@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2  // not allowed
+accountEmail = "pp@pp.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
